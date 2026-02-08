594f914f927f0b1b1cf1d733968198c718368f84
Robbie Sims <Sims@sims.MG8702>
2026-02-08 09:45:24 -0600

    Add PWA support: manifest, icons, service worker for offline + home screen install

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE = "paint-calc-v1";
+var FILES = ["/paint-calc/", "/paint-calc/index.html", "/paint-calc/manifest.json", "/paint-calc/icon-192.png", "/paint-calc/icon-512.png"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+});
